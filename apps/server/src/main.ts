c4117ac593ff383304194508cbe5c4f477bd6212
Mike Nsubuga <nsubugamike021@gmail.com>
2020-12-02 15:11:35 +0300

    added postgres api

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,15 +4,61 @@
  */
 
 import * as express from 'express';
+import * as cors from 'cors';
+import { Pool} from 'pg'
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to server!' });
-});
+app.use(express.json())
+app.use(cors())
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+const pool = new Pool({
+    "user": "postgres",
+    "password" : "postgres",
+    "host" : "localhost",
+    "port" : 5432,
+    "database" : "todo"
+})
+
+ 
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+app.get("/todos", async (req, res) => {
+    const rows = await readTodos();
+    res.setHeader("content-type", "application/json")    
+    res.send(JSON.stringify(rows))
+})
+
+
+
+
+
+
+app.listen(8080, () => console.log("Web server is listening.. on port 8080"))
+
+start()
+
+async function start() {
+    await connect();
+}
+
+async function connect() {
+    try {
+        await pool.connect(); 
+    }
+    catch(e) {
+        console.error(`Failed to connect ${e}`)
+    }
+}
+
+async function readTodos() {
+    try {
+    const results = await pool.query("select id, text from todos");
+    return results.rows;
+    }
+    catch(e){
+        return [];
+    }
+}
+
+
